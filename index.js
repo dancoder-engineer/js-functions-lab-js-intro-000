67501bffad9dd32a0a2b9a60b13cdf4f67619a91
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-03 18:11:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ function happyHolidaysTo(name) {return `Happy holidays, ${name}!` }
 
 function happyCustomHolidayTo(holiday, name) { return `Happy ${holiday}, ${name}!` }
 
+function holidayCountdown (holiday, days) { return `It's ${days} days until ${holiday}!` }
+
